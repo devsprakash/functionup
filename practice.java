a7011e8afacc23b8619fb66eef3985e5b5e55987
devPrakash7 <psamantaray77@gmail.com>
2022-06-02 08:43:33 +0530

    init

---
+++ practice.java
@@ -0,0 +1,14 @@
+class practice{
+
+    public static void main(String[] args) {
+        
+        for(int i=0;i<=5; i++){
+
+              for(int j=0;j<5; j++){
+
+                   System.out.println("*");
+              }
+              System.out.println(" ");
+        }
+    }
+}
